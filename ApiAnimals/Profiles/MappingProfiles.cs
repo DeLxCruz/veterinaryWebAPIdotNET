1ed33e04029aa6f227ea426d2e7cc15c8f6158f0
DeLxCruz <lopezsantiago3503@gmail.com>
2023-10-07 16:08:04 -0500

    feat: :wrench: Update Mapping File.

--- ApiAnimals/Profiles/MappingProfiles.cs
+++ ApiAnimals/Profiles/MappingProfiles.cs
@@ -13,5 +13,14 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Pais, PaisDto>().ReverseMap();
-    }
+        CreateMap<Cita, CitaDto>().ReverseMap();
+        CreateMap<Ciudad, CiudadDto>().ReverseMap();
+        CreateMap<ClienteDireccion, ClienteDireccionDto>().ReverseMap();
+        CreateMap<Cliente, ClienteDto>().ReverseMap();
+        CreateMap<ClienteTelefono, ClienteTelefonoDto>().ReverseMap();
+        CreateMap<Departamento, DepartamentoDto>().ReverseMap();
+        CreateMap<Mascota, MascotaDto>().ReverseMap();
+        CreateMap<Raza, RazaDto>().ReverseMap();
+        CreateMap<Servicio, ServicioDto>().ReverseMap();
+    }   
 }
