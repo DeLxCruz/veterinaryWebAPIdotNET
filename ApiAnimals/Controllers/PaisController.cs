edaff12efc04379a57e169fa970244e6eff825ed
DeLxCruz <lopezsantiago3503@gmail.com>
2023-10-09 12:59:22 -0500

    feat: :wrench: Update PaisController

--- ApiAnimals/Controllers/PaisController.cs
+++ ApiAnimals/Controllers/PaisController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApiAnimals.Dtos;
 using AutoMapper;
+using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -27,5 +28,25 @@ public class PaisController : BaseControllerApi
     {
         var paises = await _unitOfWork.Paises.GetAllAsync();
         return _mapper.Map<List<PaisDto>>(paises);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+    public async Task<ActionResult<Pais>> Post(PaisDto paisDto)
+    {
+        var pais = _mapper.Map<Pais>(paisDto);
+        this._unitOfWork.Paises.Add(pais);
+        await _unitOfWork.SaveAsync();
+        
+        if (pais == null)
+        {
+            return BadRequest((new ApiResponse(400)));
+        }
+
+        paisDto.Id = pais.Id;
+        return CreatedAtAction(nameof(Post), new { id = paisDto.Id}, paisDto);
+
     }
 }
